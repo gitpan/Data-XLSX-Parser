1cc3c39eefa53c09ace8a27839bd9b604ba708dc
Masatoshi Kawazoe <acidlemon@cpan.org>
2014-12-18 01:56:25 +0000

    Import of ACIDLEMON/Data-XLSX-Parser-0.11.tar.gz from CPAN.
    gitpan-cpan-distribution: Data-XLSX-Parser
    gitpan-cpan-version:      0.11
    gitpan-cpan-path:         ACIDLEMON/Data-XLSX-Parser-0.11.tar.gz
    gitpan-cpan-author:       ACIDLEMON
    gitpan-cpan-maturity:     released

--- lib/Data/XLSX/Parser.pm
+++ lib/Data/XLSX/Parser.pm
@@ -2,7 +2,7 @@ package Data::XLSX::Parser;
 use strict;
 use warnings;
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 use Data::XLSX::Parser::DocumentArchive;
 use Data::XLSX::Parser::Workbook;
